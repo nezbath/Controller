22c9157cfb1280a4b51544d1de321794da71b5f3
nezbath <nmaswood@hotmail.com>
2017-05-17 22:20:39 -0400

    test

--- src/main/java/mytestid/com/nez/test/git/App.java
+++ src/main/java/mytestid/com/nez/test/git/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("This is a test");
+        System.out.println("This is a testing");
     }
 }
